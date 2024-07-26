942a6cd5cd5d099ce90d7fdbcf60b0e29bd0acb4
unknown <you@example.com>
2024-07-26 14:15:20 +0300

    four commit

--- src/components/NavBar/component/TailNav.jsx
+++ src/components/NavBar/component/TailNav.jsx
@@ -2,14 +2,16 @@
 import React from "react";
 import { Navbar } from "flowbite-react";
 import "./TailNav.modules.css";
+
+
 function TailNav() {
   return (
     <Navbar className="flex gap-x-5 bg-[#913b10] py-4 ">
-      <div className="container flex">
-        <Navbar.Toggle className="" />
+      <div className=" flex ">
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse className="container">
-        <div className="">
+        <div>
           <div className="borderYtoX flex flex-col gap-y-4 md:gap-y-0 justify-center p-4 md:p-0 mt-4 font-medium rounded-lg bg-transparent sm:space-x-2 md:space-x-4 xl:space-x-7 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent">
             <a
               href="#"
@@ -47,7 +49,10 @@ function TailNav() {
             >
               ACCESSORIES
             </a>
-            <a href="#" className="text-base md:text-sm lg:text-base font-medium text-white">
+            <a
+              href="#"
+              className="text-base md:text-sm lg:text-base font-medium text-white"
+            >
               ABOUT US
             </a>
           </div>
